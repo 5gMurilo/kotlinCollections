8825a35152fee4092e52d57cfd8e4585fa3d8765
5gMurilo <cardoso.murilo2002@gmail.com>
2022-01-10 18:15:24 -0300

    Max, min, average and filter (refactor)

--- src/main/kotlin/me/murilo/TesteOperacoes.kt
+++ src/main/kotlin/me/murilo/TesteOperacoes.kt
@@ -10,12 +10,23 @@ fun main() {
 
     println("------------")
 
-    println("Maior salario -> ${salarios.maxOf { d: Double -> d }}")
-    println("Menor salario -> ${salarios.minOf {d: Double -> d}}")
+    println("Maior salario -> ${salarios.maxOrNull()}")
+    println("Menor salario -> ${salarios.minOrNull()}")
     println("Média dos salarios -> ${salarios.average()}")
 
     val salarioMaiorQue2500 = salarios.filter { it > 2500 }
     println("------------")
-
     salarioMaiorQue2500.forEach { println(it) }
+
+    println("------------")
+    println(salarios.count {it in 2500.0..5000.0} )
+
+    println("------------")
+    val finder:Double? = salarios.find { it == 4120.00 }
+    println(finder ?: "este salário não existe na lista")
+    println(salarios.find { it == 1000.0 })
+
+    println("------------")
+    println(salarios.any{ it == 1000.0})
+
 }
