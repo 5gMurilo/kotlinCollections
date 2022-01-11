ba514f17b3837aca2b9847990339d3558ecc2b8c
5gMurilo <cardoso.murilo2002@gmail.com>
2022-01-11 18:29:15 -0300

    list with group by and sortedBy

--- src/main/kotlin/me/murilo/TesteList.kt
+++ src/main/kotlin/me/murilo/TesteList.kt
@@ -1,9 +1,9 @@
 package me.murilo
 
 fun main() {
-    val joao = Funcionario(nome = "João José", salario = 2600.0)
-    val pedro = Funcionario("Pedro Pascoal", salario = 1960.0)
-    val maria = Funcionario("Maria Marta", salario = 2600.0)
+    val joao = Funcionario(nome = "João José", salario = 2600.0, contratacao="CLT" )
+    val pedro = Funcionario("Pedro Pascoal", salario = 1960.0, contratacao="CLT" )
+    val maria = Funcionario("Maria Marta", salario = 2900.0, contratacao="PJ" )
 
     val empresa = listOf(joao, pedro, maria)
 
@@ -13,11 +13,25 @@ fun main() {
 
     println("---------------")
     println(empresa.find { it.nome == "Maria Marta"  })
+
+    println("---------------")
+    empresa
+        .sortedBy{ it.salario }
+        .forEach { println(it) }
+
+    println("---------------")
+
+    empresa
+        .groupBy{ it.contratacao }
+        .forEach { println(it) }
+
+
 }
 
 data class Funcionario(
     val nome:String,
-    val salario:Double
+    val salario:Double,
+    val contratacao:String
 ){
     override fun toString(): String =
         """
